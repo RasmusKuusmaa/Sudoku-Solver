3410e7f4330a8c49317f28bc97bf6c064cbd2fda
77f315 <77f315@gmail.com>
2024-08-29 14:46:49 +0300

    ability to change cellval, console log the grid on solve

--- src/App.js
+++ src/App.js
@@ -3,22 +3,39 @@ import './App.css';
 
 function App() {
   const [grid, setGrid] = useState(Array.from({ length: 9 }, () => Array(9).fill('')));
-  
+
+  const handleChange = (row, col, e) => {
+    const value = e.target.value;
+
+    setGrid(prevGrid => {
+      return prevGrid.map((r, rowIndex) =>
+        r.map((cell, colIndex) =>
+          rowIndex === row && colIndex === col ? value : cell
+        )
+      );
+    });
+  };
+
+
   return (
     <div className="App">
       <div className="SudokuContainer">
         {grid.map((row, rowIndex) => (
           <div key={rowIndex} className="row">
             {row.map((cell, colIndex) => (
-              <div key={colIndex} className="Cell">
-                
-                {cell}
-              </div>
+              <input 
+                key={colIndex}
+                type='text'
+                value={cell}
+                className='Cell'
+                maxLength={1}
+                onChange={(e) => handleChange(rowIndex, colIndex, e)}
+              />
             ))}
           </div>
         ))}
       </div>
-      <button>SOLVE</button>
+      <button onClick={() => console.log(grid)}>SOLVE</button>
     </div>
   );
 }
